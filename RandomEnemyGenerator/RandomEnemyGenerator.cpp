c6ad451860a1c9a7916be44f410d37c13911939d
gerardarditcastells <gerardarditcastells@S3-08>
2017-02-21 10:46:32 +0100

    Correcció

--- RandomEnemyGenerator/RandomEnemyGenerator.cpp
+++ RandomEnemyGenerator/RandomEnemyGenerator.cpp
@@ -2,8 +2,10 @@
 #include <math.h>
 #include <time.h>
 #include <iostream>
+#include <ctime>
+#include <string>
 
-enum EnemyType{zombie, vampire, ghost, witch};
+enum EnemyType{ZOMBIE, VAMPIRE, GHOST, WITCH, MAX};
 
 
 struct Enemy
@@ -12,30 +14,90 @@ struct Enemy
 
 	std::string name;
 	int health;
+
+	std::string getEnemyTypeString()
+	{
+		switch (type) {
+		case EnemyType::ZOMBIE: return "zombie";
+		case EnemyType::VAMPIRE: return "vampire";
+		case EnemyType::GHOST: 	return "ghost";
+		case EnemyType::WITCH: return "werewolf";
+		case EnemyType::MAX: return "max";
+		default: return " ";
+		}
+	}
+
+
 };
 
 bool operador(Enemy a, Enemy b) {
 
-	if (a.name == b.name && a.type == b.type) {
+	return a.name == b.name && a.type == b.type;
+
+	/*if (a.name == b.name && a.type == b.type) {           ESTA BE IGUALMENT
 
 		return true;
 	}
 	else
 	{
 		return false;
-	}
+	}*/
 }
 
-void CreateRandomEnemy() {
+Enemy CreateRandomEnemy() {
 
+	const int MAX_LIFE = 500;
+	const std::string NAMES[]{
+		"Gertrudiz",
+		"Pancracia",
+		"Anacleto",
+		"Hipolito",
+		"Eustaquio",
+		"Fulgencia"
+	};
 
-
-
-
+	return Enemy{
+		static_cast <EnemyType>(rand() % static_cast<int>(EnemyType::MAX)),
+		NAMES[rand() % (sizeof NAMES / sizeof std::string)], rand() % MAX_LIFE
+	};
 }
 
 
 int main(void) {
+
+	srand(static_cast<unsigned>(time(nullptr)));
+
+	const int MAX_ENEMIES{ 5 };
+	Enemy enemies[MAX_ENEMIES];
+
+	{
+		int i = 0;
+		while (i < MAX_ENEMIES) {
+
+			enemies[i] = CreateRandomEnemy();
+			int j = i - 1;
+			while (j >= 0)
+			{
+				if (enemies[i] == enemies[j]) {
+					--i;
+					break;
+
+				}
+			}
+		}
+
+
+	}
+
+
+	std::cout << "List of enemies:\n";
+
+	for (auto &e : enemies) {
+
+		std::cout << e.name << "is a" << e.getEnemyTypeString() << "whose life is" << e.health << std::endl;
+ 
+	}
+
 
 
 }
