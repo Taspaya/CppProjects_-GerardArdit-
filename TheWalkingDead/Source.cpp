254955a32ec6b44ea6c05786831786f7d53adc8d
gerardarditcastells <gerardarditcastells@S3-13>
2017-02-28 10:35:58 +0100

    correcció cutre

--- TheWalkingDead/Source.cpp
+++ TheWalkingDead/Source.cpp
@@ -4,54 +4,70 @@
 
 enum Weapon { FISTS, GUN, SHOTGUN, REVOLVER, SNIPER, MACHINE_GUN, MAX};
 
+
 class Zombie;
 
 
-class Player 
+class Player              // AIXÒ NORMALMENT HAURIA D'ANAR AL FITXER .h
 {
 public:
 	Weapon weapon;
-	float precision; 
-	int life;	
-	
-	
-	Player() {
-		precision= std::rand() % MAX;
-	}
+	float precision;
+	int life;
+	int attack;
+	Player() :																								  // CONSTRUCTOR	(sempre ha de ser públic)
+		weapon(static_cast<Weapon>(rand() % static_cast<int>(Weapon::MAX))),								 //
+		precision((rand() % 10 / 10.f)),																	//
+		life(100) {};																					   //
+																										  //
+																									     //
+	Player(const Weapon &t_weapon, float t_precision, int t_life) :										// DESTRUCTOR
+		weapon(t_weapon),
+		precision(t_precision),
+		life(t_life) {};
 
-	void attack(Zombie &);
 
+	void attack(Zombie &zombie) const;
+	bool isAlive() const;
 };
 
-Player::Player() : weapon(static_cast <Weapon>(rand() % static_cast <int> (Weapon::MAX))), precision((rand() % 11 / 10.0)), life(100) {}
+//Player::Player() : weapon(static_cast <Weapon>(rand() % static_cast <int> (Weapon::MAX))), precision((rand() % 11 / 10.0)), life(100){}
 
 class Zombie {
 
 public:
-	int distanceToPlayer;
-	Zombie() {};
-	
+	int distanceToPlayer;	
 	float speed;
 	float damage;
 	int life;
+	
+	Zombie():
+		distanceToPlayer(rand() % 180 + 20),
+		speed((rand() % 20)), 
+		damage(rand() % 20 / 10.f), life(100){}
+
 	void attack(Player &);
 
 };
 
-void Player::attack(Zombie &a) {
+//Zombie::Zombie() : distanceToPlayer(rand() % 180 + 20), speed(rand() % 20), damage(rand() % 20 / 10.0), life(100) {}
 
-	
-	if (a.distanceToPlayer <= 0){
-		a.life = a.life - (static_cast<int>(weapon)*precision);
-	}
+void Player::attack(Zombie &zombie)
+{
+
+	zombie.life -= static_cast<int>(static_cast<int>(weapon)* precision);
+	/*if (zombie.distanceToPlayer <= 0){
+		zombie.life = zombie.life - (static_cast<int>(weapon)*precision);
+	}*/
+}
+
+bool Player::isAlive() const
+{
+	return life > 0;
 }
 
 void Zombie::attack(Player &z) {
 	
-	//	Si la distancia hasta el jugador es igual o menor que 0
-	//	entonces se resta el daño del zombie a la vida del jugador.En caso
-	//	contrario, el zombie adelanta un paso más hacia el jugador.
-
 
 	if (z.attack <= 0) {
 		z.life = z.life - (static_cast<int>(Weapon)*precision);
@@ -59,7 +75,7 @@ void Zombie::attack(Player &z) {
 
 }
 
-Zombie::Zombie() : distanceToPlayer(rand() % 180 + 20), speed(rand() % 20), damage(rand() % 20 / 10.0), life(100) {}
+
 
 bool IsAlive(){
 
@@ -69,10 +85,43 @@ bool IsAlive(){
 
 void main() {
 
-	Player p;
+	srand(static_cast<unsigned>(time(nullptr)));
+
+	Player pepito;
+	const int MAX_ZOMBIES{ 10 };
+	Zombie zombies[MAX_ZOMBIES];
+
+	std::cout << "Player \n\tinitial life:" << pepito.life << ", weapon: " << pepito.weapon << ", precision:" << pepito.precision << std::endl;
+
+	std::cout << "ZOMBIES ARE COMING!" << std::endl;
+
+	bool zombiesAreAlive;
+	do {
+		zombiesAreAlive = false;
+		
+		for (int i = 0; i < MAX_ZOMBIES; ++i) 
+		{
+			std::cout << "Zombie[" << i << "] \n\tlife:" << zombies[i].life << ", distance: " << zombies[i].distanceToPlayer << ", speed";
+
+
+
+		}
+
+
+
+
+
+
+	} while (pepito.isAlive() && zombiesAreAlive);
+
+	if (zombiesAreAlive)
+		std::cout << "GAME OVER";
 	
 
 	
-}
+	}
+
+	
+
 
 
