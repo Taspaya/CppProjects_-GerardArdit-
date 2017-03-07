068e2757ef7ca914297de05841e76dd1369882ee
gerardarditcastells <gerardarditcastells@S3-13>
2017-03-07 10:59:42 +0100

    exemple d'objectes d'una classe circle

---
+++ Testing/testing.cc
@@ -0,0 +1,18 @@
+#include "circle.h"
+
+
+int main() {
+	Circle c1(2.0, 3.0, 4.0); // c1 es un objece de la classe Circle
+
+	Cicle c3(2.0, 3.0, 4.0)); // c3 es un objecte de la classe Circle
+
+	Circle *c2;
+	c2 = new Circle(1.9, 2.0, 3.0); // c2 es un punter que apunta a un objecre de la classe circle
+	delete c2;
+	c2 = nullptr;
+
+		return 0,
+
+
+
+}
